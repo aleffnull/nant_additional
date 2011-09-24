71bd12c12c7e447af7742828d29902e94bb05b86
Mikhail Savkin <savkinmk@gmail.com>
2011-09-24 23:35:08 +0400

    Creating solution skeleton

---
+++ sources/NAnt.Additional.Tasks/XmlDropNodeTask.cs
@@ -0,0 +1,16 @@
+﻿using NAnt.Core;
+
+namespace NAnt.Additional.Tasks
+{
+	public class XmlDropNodeTask : Task
+	{
+		#region Task overrides
+
+		protected override void ExecuteTask()
+		{
+			//
+		}
+
+		#endregion Task overrides
+	}
+}
